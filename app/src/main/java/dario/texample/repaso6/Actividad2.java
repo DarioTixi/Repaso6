3a2b4cfebaeb748b8594bf32cf549d3050ecbfe2
dario <dario.moya2016@gmail.com>
2022-05-22 22:11:38 -0500

    Save hasta ma;ana

--- app/src/main/java/dario/texample/repaso6/Actividad2.java
+++ app/src/main/java/dario/texample/repaso6/Actividad2.java
@@ -2,13 +2,36 @@ package dario.texample.repaso6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Actividad2 extends AppCompatActivity {
+private Button regresar;
+private EditText edtnombre;
+private  EditText edtBase;
+private  EditText edtExponente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad2);
+        regresar= findViewById(R.id.buttonRegresar);
+        edtnombre= findViewById(R.id.editTextNombre1);
+        Bundle bundle = this.getIntent().getExtras();
+        edtnombre.setText(bundle.getString("nombre_usuario"));
+        edtExponente = findViewById(R.id.editTextExponente2);
+        edtBase = findViewById(R.id.editTextBase3);
+
+    }
+
+    public void Regresar(View view){
+
+
+        Intent intent = new Intent(this,MainActivity.class);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/dario/texample/repaso6/MainActivity.java
+++ app/src/main/java/dario/texample/repaso6/MainActivity.java
@@ -2,25 +2,42 @@ package dario.texample.repaso6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-private EditText nombre;
+private EditText editTnombre;
 private Button enviar;
+private Button calcular;
+private EditText base1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    nombre= findViewById(R.id.editTextNombre);
+        editTnombre= findViewById(R.id.editTextNombre);
     enviar= findViewById(R.id.button_Enviar);
-    if(nombre.getText().toString()==""){
-        enviar.setEnabled(false);
+    calcular= findViewById(R.id.button_Calcular);
+
+
+
+   if(editTnombre.getText().toString().equals("")){
+        calcular.setEnabled(false);
     }else{
-        enviar.setEnabled(true);
+        calcular.setEnabled(true);
     }
+
     }
+     public  void Enviar(View view){
+         Intent intent= new Intent(this,Actividad2.class);
+         String nombreenviar= editTnombre.getText().toString();
+         intent.putExtra("nombre_usuario",nombreenviar);
+         startActivity(intent);
+
+     }
+
 
 }
