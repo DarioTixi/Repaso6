3e0124bbd3b24ab241899364151ca778beba6587
dario <dario.moya2016@gmail.com>
2022-05-22 20:34:10 -0500

    Hola

--- app/src/main/java/dario/texample/repaso6/MainActivity.java
+++ app/src/main/java/dario/texample/repaso6/MainActivity.java
@@ -3,12 +3,24 @@ package dario.texample.repaso6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+private EditText nombre;
+private Button enviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    nombre= findViewById(R.id.editTextNombre);
+    enviar= findViewById(R.id.button_Enviar);
+    if(nombre.getText().toString()==""){
+        enviar.setActivated(false);
+    }else{
+        enviar.setActivated(true);
     }
+    }
+
 }
